7fb6d756bd282a67ea34a89c94828361aa09833b
casmita24 <asmita24.work@gmail.com>
2024-03-17 16:20:32 +0530

    Data Tranformation

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,56 @@
+import os
+import sys
+import pandas as pd
+
+from src.exceptions import CustomException
+from src.logger import logging
+from src.components.data_transformation import DataTransformation
+
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+
+@dataclass
+class DataIngestionConfig:
+    raw_data_path: str = os.path.join('artifacts', 'data.csv')
+    train_data_path: str = os.path.join('artifacts', 'train.csv')
+    test_data_path: str = os.path.join('artifacts', 'test.csv')
+
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info('Entered Data Ingestion Method')
+        
+        try:
+            df = pd.read_csv('notebook\data\study.csv')
+            logging.info('Read dataset')
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+
+            train_set, test_set = train_test_split(df, train_size=0.2, random_state=42)
+            logging.info('Data splitted into train test sets')
+
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+            logging.info('Data Ingestion created')
+
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+        
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+
+if __name__ == '__main__':
+    obj=DataIngestion()
+    train_data_path, test_data_path = obj.initiate_data_ingestion()
+
+    transformator = DataTransformation()
+    transformator.initiate_data_transformation(train_data_path, test_data_path)
