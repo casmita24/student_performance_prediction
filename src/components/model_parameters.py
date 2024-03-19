e82e4ee5ca263ab8681d4caa0ed9d1b33dc009ea
casmita24 <asmita24.work@gmail.com>
2024-03-19 16:06:22 +0530

    Code Optimization

---
+++ src/components/model_parameters.py
@@ -0,0 +1,64 @@
+from dataclasses import dataclass
+from catboost import CatBoostRegressor
+from xgboost import XGBRFRegressor
+from sklearn.ensemble import (
+    AdaBoostRegressor,
+    GradientBoostingRegressor,
+    RandomForestRegressor
+)
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+
+
+@dataclass
+class ModelsConfig:
+    models = {
+                'Catboosting Regressor': CatBoostRegressor(),
+                'XGBRF Regressor': XGBRFRegressor(),
+                'AdaBoost Regressor': AdaBoostRegressor(),
+                'Gradient Boosting Regressor': GradientBoostingRegressor(),
+                'Random Forest Regressor': RandomForestRegressor(),
+                'Linear Regressor': LinearRegression(),
+                'Decision Tree Regressor': DecisionTreeRegressor()
+            }
+    
+
+@dataclass
+class ModelParametersConfig:
+    params={
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                
+            }
--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -2,21 +2,13 @@ import os
 import sys
 
 from dataclasses import dataclass
-from catboost import CatBoostRegressor
-from xgboost import XGBRFRegressor
-from sklearn.ensemble import (
-    AdaBoostRegressor,
-    GradientBoostingRegressor,
-    RandomForestRegressor
-)
-from sklearn.linear_model import LinearRegression
+
 from sklearn.metrics import r2_score
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.tree import DecisionTreeRegressor
 
 from src.exceptions import CustomException
 from src.logger import logging
 from src.utils import save_object, evaluate_models
+from src.components.model_parameters import ModelsConfig, ModelParametersConfig
 
 @dataclass
 class ModelTrainerConfig:
@@ -26,6 +18,8 @@ class ModelTrainerConfig:
 class ModelTrainer:
     def __init__(self):
         self.model_trainer_config = ModelTrainerConfig()
+        self.models_config = ModelsConfig()
+        self.model_parameters_config = ModelParametersConfig()
 
     def initiate_model_trainer(self, train_array, test_array):
         try:
@@ -35,53 +29,9 @@ class ModelTrainer:
                 test_array[:, :-1], 
                 test_array[:, -1]
             )
-            models = {
-                'Catboosting Regressor': CatBoostRegressor(),
-                'XGBRF Regressor': XGBRFRegressor(),
-                'AdaBoost Regressor': AdaBoostRegressor(),
-                'Gradient Boosting Regressor': GradientBoostingRegressor(),
-                'Random Forest Regressor': RandomForestRegressor(),
-                'Linear Regressor': LinearRegression(),
-                'Decision Tree Regressor': DecisionTreeRegressor()
-            }
 
-            params={
-                "CatBoosting Regressor":{
-                    'depth': [6,8,10],
-                    'learning_rate': [0.01, 0.05, 0.1],
-                    'iterations': [30, 50, 100]
-                },
-                "XGBRegressor":{
-                    'learning_rate':[.1,.01,.05,.001],
-                    'n_estimators': [8,16,32,64,128,256]
-                },
-                "AdaBoost Regressor":{
-                    'learning_rate':[.1,.01,0.5,.001],
-                    # 'loss':['linear','square','exponential'],
-                    'n_estimators': [8,16,32,64,128,256]
-                },
-                "Gradient Boosting":{
-                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
-                    'learning_rate':[.1,.01,.05,.001],
-                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
-                    # 'criterion':['squared_error', 'friedman_mse'],
-                    # 'max_features':['auto','sqrt','log2'],
-                    'n_estimators': [8,16,32,64,128,256]
-                },
-                "Random Forest":{
-                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
-                 
-                    # 'max_features':['sqrt','log2',None],
-                    'n_estimators': [8,16,32,64,128,256]
-                },
-                "Linear Regression":{},
-                "Decision Tree": {
-                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
-                    # 'splitter':['best','random'],
-                    # 'max_features':['sqrt','log2'],
-                },
-                
-            }
+            models = self.models_config.models
+            params = self.model_parameters_config.params
 
             model_report:dict = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models, params=params)
 
